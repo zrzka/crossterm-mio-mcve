9c08fa162789d612c75c0f8dcec341ba25196876
Robert Vojta <rvojta@me.com>
2019-11-27 09:20:59 +0100

    mio-0.7
    Signed-off-by: Robert Vojta <rvojta@me.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use std::io::{ErrorKind, Result};
+use std::io::{stdin, ErrorKind, Result};
+use std::os::unix::io::AsRawFd;
 use std::time::{Duration, Instant};
 
 use crossterm::screen::RawScreen;
 use libc;
-use mio::{unix::EventedFd, Events, Poll, PollOpt, Ready, Token};
+use mio::{unix::SourceFd, Events, Interests, Poll, Token};
 
 const STDIN_TOKEN: Token = Token(0);
 const STDIN_BUFFER_SIZE: usize = 1_024;
@@ -16,16 +17,15 @@ fn main() -> Result<()> {
 
     let mut _raw = RawScreen::into_raw_mode().map_err(|_| ErrorKind::Other)?;
 
-    let poll = Poll::new()?;
+    let mut poll = Poll::new()?;
 
-    let stdin_raw_fd = libc::STDIN_FILENO;
-    let stdin_evented = EventedFd(&stdin_raw_fd);
-    poll.register(
-        &stdin_evented,
-        STDIN_TOKEN,
-        Ready::readable(),
-        PollOpt::level(),
-    )?;
+    let stdin = stdin();
+    let stdin_raw_fd = stdin.as_raw_fd();
+
+    let stdin_source = SourceFd(&stdin_raw_fd);
+
+    poll.registry()
+        .register(&stdin_source, STDIN_TOKEN, Interests::READABLE)?;
 
     let mut events = Events::with_capacity(16);
     let mut buffer = [0u8; STDIN_BUFFER_SIZE];
@@ -44,34 +44,31 @@ fn main() -> Result<()> {
             poll_call_count += 1;
         }
 
-        let count = poll.poll(&mut events, Some(Duration::from_secs(0)))?;
+        poll.poll(&mut events, Some(Duration::from_secs(0)))?;
+        let tokens = events.iter().map(|e| e.token()).collect::<Vec<Token>>();
 
-        if count > 0 {
-            let tokens = events.iter().map(|e| e.token()).collect::<Vec<Token>>();
+        if !tokens.contains(&STDIN_TOKEN) {
+            continue;
+        }
 
-            if !tokens.contains(&STDIN_TOKEN) {
-                continue;
-            }
+        poll_stdin_event_count += 1;
 
-            poll_stdin_event_count += 1;
+        let read_count = unsafe {
+            libc::read(
+                stdin_raw_fd,
+                buffer.as_mut_ptr() as *mut libc::c_void,
+                STDIN_BUFFER_SIZE as libc::size_t,
+            ) as isize
+        };
 
-            let read_count = unsafe {
-                libc::read(
-                    stdin_raw_fd,
-                    buffer.as_mut_ptr() as *mut libc::c_void,
-                    STDIN_BUFFER_SIZE as libc::size_t,
-                ) as isize
-            };
+        if read_count == -1 {
+            continue;
+        }
 
-            if read_count == -1 {
-                continue;
-            }
+        total_bytes_count += read_count as usize;
 
-            total_bytes_count += read_count as usize;
-
-            if buffer[..read_count as usize].contains(&EXCLAMATION_MARK) {
-                break;
-            }
+        if buffer[..read_count as usize].contains(&EXCLAMATION_MARK) {
+            break;
         }
     }
 
